5dad4b50dad2c6151d598550456026d0cbf58303
studiokaiji <studiokaiji@gmail.com>
2023-12-14 06:51:13 +0900

    marginを削除

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="mx-auto my-2 md:my-7 p-4 max-w-7xl w-full">{children}</div>
-  );
+  return <div className="mx-auto p-4 max-w-7xl w-full">{children}</div>;
 };
