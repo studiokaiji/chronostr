378906e37dab1cd722785808ec1408760a7bd24c
studiokaiji <studiokaiji@gmail.com>
2023-12-14 06:51:19 +0900

    アイコンを追加

---
+++ src/components/icons/close-icon.tsx
@@ -0,0 +1,7 @@
+export const CloseIcon = (props: JSX.IntrinsicElements["svg"]) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+      <path d="M3.2 23L1 20.8L9.8 12L1 3.2L3.2 1L12 9.8L20.8 1L23 3.2L14.2 12L23 20.8L20.8 23L12 14.2L3.2 23Z" />
+    </svg>
+  );
+};
