b5b5089d6850bded4118c94ceb070647fef946b8
studiokaiji <studiokaiji@gmail.com>
2023-12-13 12:10:11 +0900

    propsを追加

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,5 +1,10 @@
-export const Spinner = () => {
+export const Spinner = (props: JSX.IntrinsicElements["div"]) => {
   return (
-    <div className="animate-spin w-4 h-4 border-2 border-t-transparent rounded-full"></div>
+    <div
+      {...props}
+      className={`animate-spin w-4 h-4 border-2 border-t-transparent rounded-full ${
+        props.className || ""
+      }`}
+    ></div>
   );
 };
