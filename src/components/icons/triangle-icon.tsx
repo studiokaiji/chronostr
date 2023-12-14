a7d3bea8df20f858c569185afa4357c3afba6162
studiokaiji <studiokaiji@gmail.com>
2023-12-15 07:08:24 +0900

    アイコンを追加

---
+++ src/components/icons/triangle-icon.tsx
@@ -0,0 +1,7 @@
+export const TriangleIcon = (props: JSX.IntrinsicElements["svg"]) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 22" {...props}>
+      <path d="M0 22L12 1L24 22H0ZM4.32 19.375H19.68L12 5.92188L4.32 19.375Z" />
+    </svg>
+  );
+};
