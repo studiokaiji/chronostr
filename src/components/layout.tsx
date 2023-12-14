1f1c38298088e200c11ade08b8d711ed02e55fda
studiokaiji <studiokaiji@gmail.com>
2023-12-14 10:32:37 +0900

    max-wを狭めた

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="mx-auto p-4 max-w-7xl w-full">{children}</div>;
+  return <div className="mx-auto p-4 max-w-5xl w-full">{children}</div>;
 };
