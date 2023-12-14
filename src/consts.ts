4914b72e0829ccd1abf921850afc98d4a26b6bb5
studiokaiji <studiokaiji@gmail.com>
2023-12-14 10:31:37 +0900

    relayのミスタイプを修正

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ export const DRAFT_TIME_BASED_CALENDAR_EVENT_KIND = 31927;
 export const DRAFT_CALENDAR_KIND = 31928;
 export const CALENDAR_EVENT_RSVP_KIND = 31925;
 export const DEFAULT_RELAYS = [
-  "wss://127.0.0.1:7777",
+  "ws://127.0.0.1:7777",
   /*
   "wss://yabu.me",
   "wss://nos.lol",
