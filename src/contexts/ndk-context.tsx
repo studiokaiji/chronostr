825f41ba0e12bc40fa9ed1b7723166cc3a9032bc
studiokaiji <studiokaiji@gmail.com>
2023-12-15 14:36:08 +0900

    接続状態の確認を最低限行うように実装

--- src/contexts/ndk-context.tsx
+++ src/contexts/ndk-context.tsx
@@ -1,9 +1,13 @@
-import NDK from "@nostr-dev-kit/ndk";
+import { AppLocalStorage } from "@/services/app-local-storage";
+import { getRelays } from "@/services/relays";
+import NDK, { NDKNip07Signer, NDKSigner } from "@nostr-dev-kit/ndk";
 import {
   Dispatch,
   ReactNode,
   SetStateAction,
   createContext,
+  useEffect,
+  useRef,
   useState,
 } from "react";
 
@@ -12,8 +16,45 @@ export const setNDKContext = createContext<
   Dispatch<SetStateAction<NDK | null>>
 >(() => undefined);
 
+const appStorage = new AppLocalStorage();
+
 export const NDKContextProvider = ({ children }: { children: ReactNode }) => {
   const [ndk, setNDK] = useState<NDK | null>(null);
+  const isCalledRef = useRef(false);
+
+  useEffect(() => {
+    if (!ndk && !isCalledRef.current) {
+      isCalledRef.current = true;
+
+      let signer: NDKNip07Signer | undefined = undefined;
+
+      const strConnected = appStorage.getItem("connected");
+
+      if (strConnected && JSON.parse(strConnected.toLowerCase())) {
+        signer = new NDKNip07Signer();
+        signer
+          .blockUntilReady()
+          .then(() => createNewNDK(signer))
+          .catch(() => createNewNDK());
+      } else {
+        createNewNDK();
+      }
+    }
+  }, []);
+
+  const createNewNDK = async (signer?: NDKSigner) => {
+    const newNDK = new NDK({
+      explicitRelayUrls: getRelays(),
+      signer,
+    });
+    await newNDK.connect();
+
+    setNDK(newNDK);
+
+    appStorage.setItem("connected", String(!!signer));
+
+    return newNDK;
+  };
 
   return (
     <ndkContext.Provider value={ndk}>
--- src/hooks/use-ndk.ts
+++ src/hooks/use-ndk.ts
@@ -1,8 +1,8 @@
 import { ndkContext, setNDKContext } from "@/contexts/ndk-context";
 import { AppLocalStorage } from "@/services/app-local-storage";
 import { getRelays } from "@/services/relays";
-import NDK, { NDKNip07Signer, NDKSigner } from "@nostr-dev-kit/ndk";
-import { useContext, useEffect, useState } from "react";
+import NDK, { NDKNip07Signer } from "@nostr-dev-kit/ndk";
+import { useContext, useState } from "react";
 
 const appStorage = new AppLocalStorage();
 
@@ -12,55 +12,22 @@ export const useNDK = () => {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (!ndk) {
-      try {
-        let signer: NDKNip07Signer | undefined = undefined;
-
-        const strConnected = appStorage.getItem("connected");
-
-        if (strConnected && JSON.parse(strConnected.toLowerCase())) {
-          signer = new NDKNip07Signer();
-        }
-        createNewNDK(signer);
-      } catch (e) {
-        console.log(e);
-        createNewNDK();
-        appStorage.setItem("connected", String(false));
-      }
-    }
-  }, []);
-
   const connectToNip07 = async () => {
     setIsLoading(true);
+
     const newNDK = new NDK({
       explicitRelayUrls: getRelays(),
       signer: new NDKNip07Signer(),
     });
+
     await newNDK.connect();
 
     setNDK(newNDK);
     setIsLoading(false);
+
     appStorage.setItem("connected", String(true));
-  };
 
-  const createNewNDK = (signer?: NDKSigner) => {
-    setIsLoading(true);
-
-    const newNDK = new NDK({
-      explicitRelayUrls: getRelays(),
-      signer,
-    });
-
-    newNDK
-      .connect()
-      .then(() => {
-        setNDK(newNDK);
-      })
-      .catch(() => {})
-      .finally(() => {
-        setIsLoading(false);
-      });
+    return newNDK;
   };
 
   return { ndk, isLoading, connectToNip07 };
