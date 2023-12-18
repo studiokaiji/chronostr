a8490475498754b3bb17eb978389888cc218467c
studiokaiji <studiokaiji@gmail.com>
2023-12-18 21:46:40 +0900

    add CalendarTable

---
+++ src/components/calendar-table.tsx
@@ -0,0 +1,91 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { CircleIcon } from "@/components/icons/circle-icon";
+import { CloseIcon } from "@/components/icons/close-icon";
+import { TriangleIcon } from "@/components/icons/triangle-icon";
+import type { EventCalendar, GetRSVPResponse } from "@/event";
+import { formatDate } from "@/lib/formatDate";
+
+type CalendarProps = {
+  calendar: EventCalendar;
+  rsvp: GetRSVPResponse;
+};
+
+export const CalendarTable = ({
+  calendar,
+  rsvp: { rsvpPerUsers, totals },
+}: CalendarProps) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow className="rounded-t-md">
+          <TableHead>Name</TableHead>
+          {calendar?.dates.map(({ id, date, includeTime }) => (
+            <TableHead id={id}>
+              <div>{formatDate(date)}</div>
+              {includeTime && (
+                <div>
+                  {date.getHours()}:{("0" + date.getMinutes()).slice(-2)}
+                </div>
+              )}
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {Object.values(rsvpPerUsers).map((rsvp) => {
+          return (
+            <TableRow key={rsvp.user.profile?.name}>
+              <TableCell>
+                <div className="max-w-[140px] overflow-hidden text-ellipsis">
+                  {rsvp.user.profile?.displayName ||
+                    rsvp.user.profile?.name ||
+                    rsvp.user.npub}
+                </div>
+              </TableCell>
+              {calendar.dates.map((date) => {
+                const statusAndEvent = rsvp.rsvp?.[date.id];
+                const status = statusAndEvent?.status;
+                return (
+                  <TableCell key={`${rsvp.user.pubkey}.${date.id}`}>
+                    {status === "accepted" ? (
+                      <CircleIcon className="fill-lime-600 w-5" />
+                    ) : status === "tentative" ? (
+                      <TriangleIcon className="fill-yellow-500 w-5" />
+                    ) : (
+                      <CloseIcon className="fill-gray-300 w-5" />
+                    )}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell>Total</TableCell>
+          {totals.map((total, i) => (
+            <TableCell key={`total-${i}`}>
+              <div className="flex items-center space-x-1">
+                <CircleIcon className="fill-lime-600 w-3.5" />
+                <div>{total.accepted}</div>
+              </div>
+              <div className="flex items-center space-x-1">
+                <TriangleIcon className="fill-yellow-500 w-3.5" />
+                <div>{total.tentative}</div>
+              </div>
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+};
