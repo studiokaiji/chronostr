1a535d494814844cfc8fd496ada70031336022d6
studiokaiji <studiokaiji@gmail.com>
2023-12-20 17:16:40 +0900

    DEFAULT_RELAYSを変更

--- src/consts.ts
+++ src/consts.ts
@@ -3,13 +3,10 @@ export const DRAFT_TIME_BASED_CALENDAR_EVENT_KIND = 31927;
 export const DRAFT_CALENDAR_KIND = 31928;
 export const CALENDAR_EVENT_RSVP_KIND = 31925;
 export const DEFAULT_RELAYS = [
-  "ws://127.0.0.1:7777",
   "wss://yabu.me",
   "wss://relay.nostr.band",
-  // "wss://nhlck50h-7777.asse.devtunnels.ms",
-  // "wss://yabu.me",
-  // "wss://nos.lol",
-  //  "wss://relay.nostr.band",
-  // "wss://relay.damus.io",
-  // "wss://nostr.mom",
+  "wss://nos.lol",
+  "wss://relay.nostr.band",
+  "wss://relay.damus.io",
+  "wss://nostr.mom",
 ];
